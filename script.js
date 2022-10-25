1177d04a43e4b2f5a49d9b6fcb25317710a5d259
Vini Guimaraes <niciusfg@gmail.com>
2022-10-25 10:18:38 -0700

    animations js not done

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-const target = document.querySelectorAll('[data-anime]');
-const animationClass = 'animate';
+// ===== SCROLL ANIMATION =======
 
-function animeScroll() {
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
-    target.forEach(function(element){
-        if((windowTop) > element.offsetTop){
-                element.classList.add(animationClass);
-            } else {
-                element.classList.remove(animationClass);
-            }
-        console.log("element", element.offsetTop)
-    })
-}
+// const target = document.querySelectorAll('[data-anime]');
+// const animationClass = 'animate';
 
-window.addEventListener('scroll' , function(){
-    animeScroll();
-})
+// function animeScroll() {
+//     const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+//     target.forEach(function(element){
+//         if((windowTop) > element.offsetTop){
+//                 element.classList.add(animationClass);
+//             } else {
+//                 element.classList.remove(animationClass);
+//             }
+//         console.log("element", element.offsetTop)
+//     })
+// }
+
+// window.addEventListener('scroll' , function(){
+//     animeScroll();
+// })
+
+// ===== MAIN BANNER TEXT ANIMATIONS =======
+
+
+const titulo = document.querySelector('h1')
+console.log("title", titulo)
