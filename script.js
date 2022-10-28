da36ad0389cf6fae0389c9989fbcfee20a821ce1
Vini Guimarães <niciusfg@gmail.com>
2022-10-28 16:39:30 -0700

    scroll animations done

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 // ===== SCROLL ANIMATION =======
 
-// const target = document.querySelectorAll('[data-anime]');
-// const animationClass = 'animate';
+const target = document.querySelectorAll("[data-anime]");
+const animationClass = "animate";
 
-// function animeScroll() {
-//     const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
-//     target.forEach(function(element){
-//         if((windowTop) > element.offsetTop){
-//                 element.classList.add(animationClass);
-//             } else {
-//                 element.classList.remove(animationClass);
-//             }
-//         console.log("element", element.offsetTop)
-//     })
-// }
+function animeScroll() {
+  const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+  console.log("target", target);
+  target.forEach(function (element) {
+    if (windowTop > element.offsetTop) {
+      console.log("here");
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+    console.log("element", element.offsetTop);
+  });
+}
 
-// window.addEventListener('scroll' , function(){
-//     animeScroll();
-// })
+window.addEventListener("scroll", function () {
+  animeScroll();
+});
 
 // ===== MAIN BANNER TEXT ANIMATIONS =======
 
-
-const titulo = document.querySelector('h1')
-console.log("title", titulo)
+const titulo = document.querySelector("h1");
+console.log("title", titulo);
