4743cf354a38ba521885935621f0575b45e509ec
Vini Guimaraes <niciusfg@gmail.com>
2022-10-29 22:21:25 -0700

    fixes erros

--- script.js
+++ script.js
@@ -5,15 +5,12 @@ const animationClass = "animate";
 
 function animeScroll() {
   const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
-  console.log("target", target);
   target.forEach(function (element) {
     if (windowTop > element.offsetTop) {
-      console.log("here");
       element.classList.add(animationClass);
     } else {
       element.classList.remove(animationClass);
     }
-    console.log("element", element.offsetTop);
   });
 }
 
